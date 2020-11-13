ede7d3d93c540ac63f45c1530ccbe0345338dec3
jwolcott <jeanne.wolcott@gmail.com>
2020-11-13 16:42:25 -0700

    added boot project

---
+++ RunEvents/src/test/java/com/skilldistillery/run/RunEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.run;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
